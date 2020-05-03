972d8eba96d9804e30bab9d96d932a784521921b
pawel <p.lagodzinski@outlook.com>
2020-05-03 13:11:38 +0200

    add optimistic lock exception support

--- src/main/java/p/plagodzinski/blogengine/application/api/BlogEngineControllerAdvice.java
+++ src/main/java/p/plagodzinski/blogengine/application/api/BlogEngineControllerAdvice.java
@@ -11,47 +11,55 @@ import p.plagodzinski.blogengine.application.exceptions.NotFoundPostException;
 import p.plagodzinski.blogengine.application.exceptions.NotFoundReviewException;
 import p.plagodzinski.blogengine.entity.dto.ExceptionDTO;
 
+import javax.persistence.OptimisticLockException;
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class BlogEngineControllerAdvice {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BlogEngineControllerAdvice.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(BlogEngineControllerAdvice.class);
 
-    @ExceptionHandler({NotFoundPostException.class})
-    public ResponseEntity<ExceptionDTO> handleNotFoundPostException(final NotFoundPostException e) {
-        return new ResponseEntity<>(
-                buildExceptionDTO("Not found post with id " + e.getPostId()), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler({NotFoundPostException.class})
+  public ResponseEntity<ExceptionDTO> handleNotFoundPostException(final NotFoundPostException e) {
+    return new ResponseEntity<>(
+            buildExceptionDTO("Not found post with id " + e.getPostId()), HttpStatus.BAD_REQUEST);
+  }
 
-    @ExceptionHandler({NotFoundReviewException.class})
-    public ResponseEntity<ExceptionDTO> handleNotFoundReviewException(
-            final NotFoundReviewException e) {
-        return new ResponseEntity<>(
-                buildExceptionDTO("Not found review with id " + e.getReviewId()), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler({NotFoundReviewException.class})
+  public ResponseEntity<ExceptionDTO> handleNotFoundReviewException(
+          final NotFoundReviewException e) {
+    return new ResponseEntity<>(
+            buildExceptionDTO("Not found review with id " + e.getReviewId()), HttpStatus.BAD_REQUEST);
+  }
 
-    @ExceptionHandler({ConstraintViolationException.class})
-    public ResponseEntity<ExceptionDTO> handleConstraintViolationException(
-            final ConstraintViolationException e) {
-        return new ResponseEntity<>(buildExceptionDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler({ConstraintViolationException.class})
+  public ResponseEntity<ExceptionDTO> handleConstraintViolationException(
+          final ConstraintViolationException e) {
+    return new ResponseEntity<>(buildExceptionDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
+  }
 
-    @ExceptionHandler(TransactionSystemException.class)
-    public ResponseEntity<ExceptionDTO> handleConstrainException(
-            final TransactionSystemException ex) {
-        if (ex.getRootCause() instanceof ConstraintViolationException) {
-            final ConstraintViolationException constraintViolationException =
-                    (ConstraintViolationException) ex.getRootCause();
-            return handleConstraintViolationException(constraintViolationException);
-        } else {
-            return new ResponseEntity<>(
-                    buildExceptionDTO("Something is wrong with database"), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  @ExceptionHandler(TransactionSystemException.class)
+  public ResponseEntity<ExceptionDTO> handleConstrainException(
+          final TransactionSystemException ex) {
+    if (ex.getRootCause() instanceof ConstraintViolationException) {
+      final ConstraintViolationException constraintViolationException =
+              (ConstraintViolationException) ex.getRootCause();
+      return handleConstraintViolationException(constraintViolationException);
+    } else {
+      return new ResponseEntity<>(
+              buildExceptionDTO(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    private ExceptionDTO buildExceptionDTO(final String errorMessage) {
-        LOGGER.error(errorMessage);
-        return new ExceptionDTO(errorMessage);
-    }
+  @ExceptionHandler(OptimisticLockException.class)
+  public ResponseEntity<ExceptionDTO> handleOptimisticLockException(
+          final OptimisticLockException ex) {
+    return new ResponseEntity<>(
+            buildExceptionDTO(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private ExceptionDTO buildExceptionDTO(final String errorMessage) {
+    LOGGER.error(errorMessage);
+    return new ExceptionDTO(errorMessage);
+  }
 }
